2605702411b008a4ab646c07984b8bc72ac7955d
Pravisti <pravisti@gmail.com>
2016-02-19 13:04:30 -0800

    make page templates

---
+++ index.js
@@ -0,0 +1,17 @@
+<script>
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#logInBtn").click(function(){
+        $("#logInModal").modal();
+    });
+});
+
+
+
+
+</script>
